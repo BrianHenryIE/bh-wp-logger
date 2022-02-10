b63b0ee5f4a8033b25e8fbecde0c4db45036baff
Brian Henry <BrianHenryIE@gmail.com>
2022-02-09 19:57:12 -0800

    Update phpstan-bootstrap.php

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Define the wp-content dir.
+ *
+ * @package brianhenryie/bh-wp-logger
+ */
 
 define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
 define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
