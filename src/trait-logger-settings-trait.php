bb582a611f59c3ea5929ea9405134c2b6cb112b2
Brian Henry <BrianHenryIE@gmail.com>
2023-04-19 18:12:03 -0700

    Update trait-logger-settings-trait.php

--- src/trait-logger-settings-trait.php
+++ src/trait-logger-settings-trait.php
@@ -61,4 +61,24 @@ trait Logger_Settings_Trait {
 		return explode( '/', $this->get_plugin_basename() )[0];
 	}
 
+	/**
+	 * The plugin basename. Used to add the Logs link on `plugins.php`.
+	 *
+	 * @see https://core.trac.wordpress.org/ticket/42670
+	 *
+	 * @throws Exception When it cannot be determined. I.e. a symlink inside a symlink.
+	 */
+	public function get_plugin_basename(): string {
+
+		// TODO: The following might work but there are known issues around symlinks that need to be tested and handled correctly.
+		// @see  https://core.trac.wordpress.org/ticket/42670
+
+		$wp_plugin_basename = plugin_basename( __DIR__ );
+
+		$plugin_filename = get_plugins( explode( '/', $wp_plugin_basename )[0] );
+
+		return array_key_first( $plugin_filename );
+
+		throw new Exception( 'Plugin installed in an unusual directory.' );
+	}
 }
