1cff49b0f97efeac55f9b7ed78875a1996a2acf1
Brian Henry <BrianHenryIE@gmail.com>
2026-02-12 23:12:30 -0800

    Autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
  * @package           brianhenryie/bh-wp-logger
  */
 
+use Alley_Interactive\Autoloader\Autoloader;
+
 $GLOBALS['project_root_dir']   = $project_root_dir  = dirname( __DIR__, 1 );
 $GLOBALS['plugin_root_dir']    = $plugin_root_dir   = $project_root_dir . '/development-plugin';
 $GLOBALS['plugin_name']        = $plugin_name       = 'bh-wp-logger-development-plugin';
@@ -10,3 +12,8 @@ $GLOBALS['plugin_name_php']    = $plugin_name_php   = $plugin_name . '.php';
 $GLOBALS['plugin_path_php']    = $plugin_root_dir . '/' . $plugin_name_php;
 $GLOBALS['plugin_basename']    = $plugin_name . '/' . $plugin_name_php;
 $GLOBALS['wordpress_root_dir'] = $project_root_dir . '/wordpress';
+
+Autoloader::generate(
+	'BrianHenryIE\\WP_Logger',
+	__DIR__ . '/wpunit',
+)->register();
