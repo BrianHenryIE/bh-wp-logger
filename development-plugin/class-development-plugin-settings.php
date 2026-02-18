225b169fd4361b45eb1b104161b4b7854d59cd47
Brian Henry <BrianHenryIE@gmail.com>
2026-02-17 22:23:28 -0800

    Delete BS development plugin files

--- development-plugin/class-development-plugin-settings.php
+++ development-plugin/class-development-plugin-settings.php
@@ -1,28 +1,4 @@
 <?php
-/**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           BH_WP_Logger_Test_Plugin
- *
- * @wordpress-plugin
- * Plugin Name:       BH WP Logger Test Plugin
- * Plugin URI:        http://github.com/username/bh-wp-logger-development-plugin/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Brian Henry
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       bh-wp-logger-development-plugin
- * Domain Path:       /languages
- */
 
 namespace BH_WP_Logger_Test_Plugin;
 
