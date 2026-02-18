2597d434e008e4a5c9b7238c05d7ee7777c864d2
Brian Henry <BrianHenryIE@gmail.com>
2026-02-17 23:01:50 -0800

    Reomv emistajes

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * Define the wp-content dir.
  *
  * @package brianhenryie/bh-wp-logger
  */
-
-define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
-define( 'WP_PLUGIN_DIR', __DIR__ . '/wp-content/plugins' );
