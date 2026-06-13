1acc170464b1a10e64c0eba9d36d8ed5365aa3ce
Brian Henry <BrianHenryIE@gmail.com>
2026-06-12 17:52:50 -0700

    `plugin_basename()` fix

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,1 +1,10 @@
 <?php
+
+/**
+ * Fix.
+ *
+ * @see plugin_basename
+ */
+global $wp_plugin_paths;
+$plugin_path = codecept_root_dir( 'development-plugin/development-plugin.php' );
+$wp_plugin_paths[ WP_PLUGIN_DIR . '/development-plugin/development-plugin.php' ] = $plugin_path;
